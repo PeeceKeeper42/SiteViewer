48e8ecc6aa316b19dba5b1f1035365fcee9d2aa2
PeeceKeeper42 <VC160420@mail.ru>
2024-08-16 16:23:45 +0300

    More CMake magic

--- sources/exampleLib.cpp
+++
@@ -1,7 +0,0 @@
-
-#include "exampleLib.hpp"
-
-int exampleLib::add(int a,int b)
-{
-    return a+b;
-}
--- sources/main.cpp
+++ sources/main.cpp
@@ -11,23 +11,29 @@
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    QNetworkAccessManager manager;
+//    QNetworkAccessManager manager;
 
-    // Подключаем сигнал finished к лямбда-функции
-    QObject::connect(&manager, &QNetworkAccessManager::finished, [](QNetworkReply *reply) {
-        if (reply->error() == QNetworkReply::NoError) {
-            QByteArray responseData = reply->readAll(); // Читаем данные ответа
-            qDebug() << "Response data:" << responseData; // Выводим в консоль
-        } else {
-            qDebug() << "Error:" << reply->errorString(); // Выводим ошибку
-        }
-        reply->deleteLater(); // Освобождаем память
-        QCoreApplication::quit(); // Завершаем приложение
-    });
+//    // Подключаем сигнал finished к лямбда-функции
+//    QObject::connect(&manager, &QNetworkAccessManager::finished, [](QNetworkReply *reply) {
+//        if (reply->error() == QNetworkReply::NoError) {
+//            QByteArray responseData = reply->readAll(); // Читаем данные ответа
+//            qDebug() << "Response data:" << responseData; // Выводим в консоль
+//        } else {
+//            qDebug() << "Error:" << reply->errorString(); // Выводим ошибку
+//        }
+//        reply->deleteLater(); // Освобождаем память
+//        QCoreApplication::quit(); // Завершаем приложение
+//    });
 
-    // Выполняем GET-запрос
-    QNetworkRequest request(QUrl("https://example.com/"));
-    manager.get(request);
+//    // Выполняем GET-запрос
+//    QNetworkRequest request(QUrl("https://example.com/"));
+//    manager.get(request);
+
+    qDebug() << CMAKE_PROJECT_NAME;
+    qDebug() << GIT_AUTHOR;
+    qDebug() << GIT_AUTHOR_LINK;
+    qDebug() << GIT_PROJECT_LINK;
+    qDebug() << BUILD_DATE;
 
     return a.exec();
 }
